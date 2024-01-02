2992a9b3286ede574b0dca0c0669f7cf3ae9a723
animeherox44 <animeherox44@gmail.com>
2024-01-01 16:25:57 -0800

    Complete problem 27.

---
+++ RemoveElement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k: int = 0 # Also tracks position of last non-val copied.
+        for index_read in range(len(nums)):
+            if (nums[index_read] != val):
+                nums[k] = nums[index_read]
+                k += 1
+        return k
