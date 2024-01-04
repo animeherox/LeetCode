2e9cc968703134acbd3035aca7350893e9540044
animeherox44 <animeherox44@gmail.com>
2024-01-04 15:46:46 -0800

    Complete problem 11.

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        leftPtr, rightPtr, maxArea = 0, len(height)-1, 0
+        while leftPtr < rightPtr:
+            minHeight = min(height[leftPtr], height[rightPtr])
+            maxArea = max(maxArea, (rightPtr-leftPtr)*minHeight)
+            if height[leftPtr] < height[rightPtr]:
+                leftPtr += 1
+            else:
+                rightPtr -= 1
+        return maxArea
