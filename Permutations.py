419e3c19bba29fe951df9d3d8dcc2ac4ea3ee28c
animeherox44 <animeherox44@gmail.com>
2024-01-09 17:33:45 -0800

    Complete problem 46.

---
+++ Permutations.py
@@ -0,0 +1,8 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if nums == []:
+            return [[]]
+        permutations = []
+        for i in range(len(nums)):
+            permutations += [[nums[i]]+partial for partial in self.permute(nums[:i]+nums[i+1:])]
+        return permutations
