62abd2b9356c429ee4206144bf7237ea3aac98c3
animeherox44 <animeherox44@gmail.com>
2024-01-11 09:00:24 -0800

    Complete problem 33.

---
+++ SearchInRotatedSortedArray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)-1
+        while start < end:
+            mid = (start + end)//2
+            # Check if midpoint is before a pivot
+            if nums[0] <= nums[mid]:
+                # If target is in range, go left
+                if nums[0] <= target <= nums[mid]:
+                    end = mid
+                else:
+                    start = mid + 1
+            # If midpoint is after pivot, the logic flips
+            else:
+                if nums[mid] < target <= nums[-1]:
+                    start = mid + 1
+                else:
+                    end = mid
+        return start if nums[start]==target else -1
