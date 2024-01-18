adb22dbdeb6c75918070722901a06e8ae77f5ded
animeherox44 <animeherox44@gmail.com>
2024-01-18 14:34:37 -0800

    Complete problem 82.

---
+++ RemoveDuplicatesFromSortedList2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        # Create dummy head to handle case where current head is removed
+        dummyHead = current = ListNode(next=head)
+        while current:
+            if current.next:
+                pointer = current.next
+                value = pointer.val
+                if pointer.next and pointer.next.val == value:
+                    # We've found a duplicate, so remove them all
+                    while pointer.next and pointer.next.val == value:
+                        pointer = pointer.next
+                    current.next = pointer.next
+                else:
+                    current = current.next
+            else:
+                current = current.next
+        return dummyHead.next
