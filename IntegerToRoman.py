f5c44c69bd533771440904ae5408cdebfa661ded
animeherox44 <animeherox44@gmail.com>
2024-01-22 09:30:35 -0800

    Complete problem 12.

---
+++ IntegerToRoman.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbols = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+        values = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+        result = ""
+        for symbol, value in zip(symbols, values):
+            while num >= value:
+                num -= value
+                result += symbol
+        return result
