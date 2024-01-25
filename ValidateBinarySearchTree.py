d5fee2bf917a2a1fa724354b0745c4d0f3843b78
animeherox44 <animeherox44@gmail.com>
2024-01-25 12:50:29 -0800

    Complete problem 98.

---
+++ ValidateBinarySearchTree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # Return whether the tree is a valid DFS, whether the left value is
+        def dfs(node):
+            if not node:
+                return True, float('inf'), -float('inf')
+            left_result, left_min, left_max = dfs(node.left)
+            if not left_result or left_max>=node.val:
+                return False, float('inf'), -float('inf')
+            right_result, right_min, right_max = dfs(node.right)
+            if not right_result or right_min<=node.val:
+                return False, float('inf'), -float('inf')
+            else:
+                return True, min(left_min, node.val), max(right_max, node.val)
+        return dfs(root)[0]
