fd6be93732cebb91a31735358d79dd14afee36cb
animeherox44 <animeherox44@gmail.com>
2024-01-28 08:37:54 -0800

    Complete problem 66.

---
+++ PlusOne.py
@@ -0,0 +1,9 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1, -1, -1):
+            digits[i] += 1
+            digits[i] %= 10
+            if digits[i] != 0:
+                return digits
+        # We only reach this point if all digits were 9
+        return [1] + digits
