c70dbd10204d93e761f57e1de72195e44011626b
animeherox44 <animeherox44@gmail.com>
2024-02-02 13:15:04 -0800

    Complete problem 901.

---
+++ LeetCode75/OnlineStockSpan.py
@@ -0,0 +1,10 @@
+class StockSpanner:
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        span_count = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span_count += self.stack.pop()[1]
+        self.stack.append((price, span_count))
+        return span_count
