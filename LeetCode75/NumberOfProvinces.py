b0cc056f6417579412789f93afe023434b5c0174
animeherox44 <animeherox44@gmail.com>
2024-02-04 11:26:30 -0800

    Complete problem 547.

---
+++ LeetCode75/NumberOfProvinces.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(currentCity: int):
+            visited[currentCity] = True
+            for neighbor, connected in enumerate(isConnected[currentCity]):
+                if connected and not visited[neighbor]:
+                    dfs(neighbor)
+        n = len(isConnected)
+        visited = [False]*n
+        count = 0
+        for city in range(n):
+            if not visited[city]:
+                count += 1
+                dfs(city)
+        return count
