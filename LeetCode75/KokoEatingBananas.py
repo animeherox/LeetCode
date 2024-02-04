610d16434a151f00c8abe7e3bd3be5d150d57868
animeherox44 <animeherox44@gmail.com>
2024-02-04 09:18:47 -0800

    Complete problem 875.

---
+++ LeetCode75/KokoEatingBananas.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        minSpeed, maxSpeed = 1, int(1e9)
+        while minSpeed < maxSpeed:
+            midSpeed = (minSpeed + maxSpeed) >> 1
+            midHours = sum((pile + midSpeed - 1) // midSpeed for pile in piles)
+            if midHours <= h:
+                maxSpeed = midSpeed
+            else:
+                minSpeed = midSpeed + 1
+        return minSpeed
