9e3b13c9870464ae489afbd98999f5c735a7dc64
animeherox44 <animeherox44@gmail.com>
2024-02-07 11:46:32 -0800

    Complete problem 2215.

---
+++ LeetCode75/FindTheDifferenceOfTwoArrays.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1, set2 = set(nums1), set(nums2)
+        diff1 = list(set1-set2)
+        diff2 = list(set2-set1)
+        return [diff1, diff2]
