081ed28866c1002688ee186a1f83dceb0a105b78
animeherox44 <animeherox44@gmail.com>
2024-02-09 10:43:07 -0800

    Complete problem 334.

---
+++ LeetCode75/IncreasingTripletSubsequence.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        small = mid = float('inf')
+        for n in nums:
+            if n > mid:
+                return True
+            if n <= small:
+                small = n
+            else:
+                mid = n
+        return False
