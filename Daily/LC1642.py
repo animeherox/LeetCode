e17ddb312802fdb8ca63d4b4121938d0d2badf0a
animeherox44 <animeherox44@gmail.com>
2024-02-17 08:06:50 -0800

    Complete problem 1642.

---
+++ Daily/LC1642.py
@@ -0,0 +1,16 @@
+from heapq import heappop, heappush
+from typing import List
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        pq = []
+        for i in range(len(heights)-1):
+            jump = heights[i+1] - heights[i]
+            if jump <= 0:
+                continue
+            heappush(pq, jump)
+            if len(pq) > ladders:
+                bricks -= heappop(pq)
+            if bricks < 0:
+                return i
+        return len(heights) - 1
