de491d1c5ab9882724ca21d42c739b6621b8351a
animeherox44 <animeherox44@gmail.com>
2024-02-18 09:33:12 -0800

    Complete problem 2149.

---
+++ Daily/LC2149.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        ans, pos, neg = [], [], []
+        for num in nums:
+            (pos if num > 0 else neg).append(num)
+        for p, n in zip(pos, neg):
+            ans += [p, n]
+        return ans
