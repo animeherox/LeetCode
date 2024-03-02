472327a8e6a7c20aa607f45d10db1c5d87a67526
animeherox44 <animeherox44@gmail.com>
2024-03-02 08:31:07 -0800

    Complete problem 606.

---
+++ Daily/LC0606.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if root is None:
+            return ""
+        else:
+            result = str(root.val)
+            if root.left:
+                result += "(" + self.tree2str(root.left) + ")"
+            elif root.right:
+                result += "()"
+            if root.right:
+                result += "(" + self.tree2str(root.right) + ")"
+            return result
