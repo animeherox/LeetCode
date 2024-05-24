599f887c2c0829c953bdf59cd22ee1f55f46c30a
animeherox44 <animeherox44@gmail.com>
2024-05-24 08:39:07 -0600

    Complete problem 1255.

---
+++ Daily/LC1255.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        count = Counter(letters)
+        def useWord(i: int) -> int:
+            isValid = True
+            earned = 0
+            for c in words[i]:
+                count[c] -= 1
+                if count[c] < 0:
+                    isValid = False
+                earned += score[ord(c) - ord('a')]
+            return earned if isValid else -1
+        
+        def unuseWord(i: int) -> None:
+            for c in words[i]:
+                count[c] += 1
+        
+        def dfs(s: int) -> int:
+            ans = 0
+            for i in range(s, len(words)):
+                earned = useWord(i)
+                if earned > 0:
+                    ans = max(ans, earned + dfs(i+1))
+                unuseWord(i)
+            return ans
+        
+        return dfs(0)
