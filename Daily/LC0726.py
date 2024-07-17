c3ffff2be48b08e0c6bebf35240b9d008192bac2
animeherox44 <animeherox44@gmail.com>
2024-07-17 13:18:05 -0700

    Complete problem 726.

---
+++ Daily/LC0726.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+        stack = [Counter()]
+        i = 0
+        while i < len(formula):
+            if formula[i] == '(':
+                stack.append(Counter())
+                i += 1
+            elif formula[i] == ')':
+                top = stack.pop()
+                i += 1
+                start = i
+                while i < len(formula) and formula[i].isdigit():
+                    i += 1
+                multiplier = int(formula[start:i] or 1)
+                for name, val in top.items():
+                    stack[-1][name] += val*multiplier
+            else:
+                start = i
+                i += 1
+                while i < len(formula) and formula[i].islower():
+                    i += 1
+                name = formula[start:i]
+                start = i
+                while i < len(formula) and formula[i].isdigit():
+                    i += 1
+                count = int(formula[start:i] or 1)
+                stack[-1][name] += count
+        return ''.join(name + (str(stack[-1][name]) if stack[-1][name] > 1 else '') for name in sorted(stack[-1]))
